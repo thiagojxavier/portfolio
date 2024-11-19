0882ede4456cd0414a1135bfa2c91a78c52f6a63
Thiago <dejesusxavierthiago@gmail.com>
2024-11-19 18:35:17 -0300

    Contact texts moved to the right

--- src/components/MeansContact.tsx
+++ src/components/MeansContact.tsx
@@ -8,7 +8,7 @@ export function MeansContact({imgLink, imgAlt, contact}:MeansContactProps) {
     return (
         <div className="contacts__contact">
             <img className="contacts__contact-icon" src={imgLink} alt={imgAlt} />
-            {contact}
+            <span className="contacts__contact-id">{contact}</span>
         </div>
     )
 }
